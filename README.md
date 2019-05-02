10f70b148286a9afebc4823ff63bc5bd1fbd181c
Anil Krishna <anil@ustwo.com>
2019-05-02 13:09:10 +1000

    first commit for the NRL match - player stats

---
+++ README.md
@@ -0,0 +1,8 @@
+# NRL Match - Top Player Stats
+
+An Android application that displays the top player stats for the given NRL match. 
+
+## Features
+
+## Tech Stack
+
---
+++ app/src/main/java/com/kayo/nrlmatchstats/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.kayo.nrlmatchstats
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
